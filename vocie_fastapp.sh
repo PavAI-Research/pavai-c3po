0c517b95cca89349d12db46855a71aa718060105
minyang chen <mychen76@gmail.com>
2024-03-05 17:50:44 -0500

    clean up

--- vocie_fastapp.sh
+++ vocie_fastapp.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-## fastapi 
+## fastapi
 python bases/pavai/vocei_web/vocei_fast.py
-print("Vocei Web FastAPI App is ready!")
-
-# import argparse
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--name", type=str, default="User")
-# args, unknown = parser.parse_known_args()
+echo "Vocei Web FastAPI App is ready!"
 
--- vocie_gradioapp.sh
+++ vocie_gradioapp.sh
@@ -2,10 +2,5 @@
 
 ## gradio mode
 gradio bases/pavai/vocei_web/vocei_app.py
-print("Vocei Web Gardio App is ready!")
-
-# import argparse
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--name", type=str, default="User")
-# args, unknown = parser.parse_known_args()
+echo "Vocei Web Gardio App is ready!"
 
