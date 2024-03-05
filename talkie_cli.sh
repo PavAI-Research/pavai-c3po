541f8aac7d045e4786e95f0f8f918a2a2c7e9130
minyang chen <mychen76@gmail.com>
2024-03-05 18:07:44 -0500

    clean up

--- talkie_cli.sh
+++ talkie_cli.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
 python bases/pavai/talkie_cli/talkie_app.py
-
-# import argparse
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--name", type=str, default="User")
-# args, unknown = parser.parse_known_args()
-
